39db0aaee496c8e0f817ffdb1e23fa40cc2d8068
Diogo Almazan <diogodpa@outlook.com>
2025-11-04 20:13:07 -0300

    Add book data handling and view templates for displaying books

---
+++ bookwise/dados.php
@@ -0,0 +1,15 @@
+<?php
+
+$livros = [
+    ['id' => 1, 'titulo' => 'Livro 1', 'autor' => 'Autor 1', 'avaliacao' => 4.5, 'descricao' => 'Descrição 1'],
+    ['id' => 2, 'titulo' => 'Livro 2', 'autor' => 'Autor 2', 'avaliacao' => 4.0, 'descricao' => 'Descrição 2'],
+    ['id' => 3, 'titulo' => 'Livro 3', 'autor' => 'Autor 3', 'avaliacao' => 3.5, 'descricao' => 'Descrição 3'],
+    ['id' => 4, 'titulo' => 'Livro 4', 'autor' => 'Autor 4', 'avaliacao' => 3.0, 'descricao' => 'Descrição 4'],
+    ['id' => 5, 'titulo' => 'Livro 5', 'autor' => 'Autor 5', 'avaliacao' => 2.5, 'descricao' => 'Descrição 5'],
+    ['id' => 6, 'titulo' => 'Livro 6', 'autor' => 'Autor 6', 'avaliacao' => 2.0, 'descricao' => 'Descrição 6'],
+    ['id' => 7, 'titulo' => 'Livro 7', 'autor' => 'Autor 7', 'avaliacao' => 1.5, 'descricao' => 'Descrição 7'],
+    ['id' => 8, 'titulo' => 'Livro 8', 'autor' => 'Autor 8', 'avaliacao' => 1.0, 'descricao' => 'Descrição 8'],
+    ['id' => 9, 'titulo' => 'Livro 9', 'autor' => 'Autor 9', 'avaliacao' => 0.5, 'descricao' => 'Descrição 9'],
+    ['id' => 10, 'titulo' => 'Livro 10', 'autor' => 'Autor 10', 'avaliacao' => 0.0, 'descricao' => 'Descrição 10'],
+]
+?>
--- bookwise/livro.php
+++ bookwise/livro.php
@@ -1,29 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookwise</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
-<body class="bg-stone-950 text-stone-300">
-    <header class=" bg-stone-900">
-        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
-            <div class="font-bold text-xl tracking-wide">Book Wise</div>
+<?php 
 
-            <ul class="flex space-x-4 font-bold">
-                <li><a href="/" class="text-lime-500">Explorar</a></li>
-                <li><a href="/meus-livros.php" class="hover:underline">Meus livros</a></li>
-            </ul>
-            <ul>
-                <li><a href="/login.php" class="hover:underline">Fazer Login</a></li>
-            </ul>
-        </nav>
-    </header>
-    
+require 'dados.php';
 
-    <main class="mx-auto max-w-screen-lg space-y-6">
+$id = $_REQUEST['id'];
 
-    </main>
-</body>
-</html>
+$filtrado = array_filter($livros, fn($l) => $l['id'] == $id);
+
+$livro = array_pop($filtrado);
+
+$view = 'livro';
+require 'views/template/app.php';
+?>
