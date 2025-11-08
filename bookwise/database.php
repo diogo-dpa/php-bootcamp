204c8729f12e1c3d213faa9a37ec9115409899bd
Diogo Almazan <diogodpa@outlook.com>
2025-11-08 16:06:26 -0300

    Refactor database queries in DB class to utilize prepared statements and fetch Livro objects directly; remove static make method from Livro model for cleaner object instantiation.

--- bookwise/database.php
+++ bookwise/database.php
@@ -18,21 +18,23 @@ class DB {
         $prepare = $this->db->prepare("select * from livros where usuario_id = 1 and titulo like :pesquisa");
 
         $prepare->bindValue('pesquisa', "%$pesquisa%");
+        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
         $prepare->execute();
 
-        $items = $prepare->fetchAll();
-
-        return array_map(fn($item) => Livro::make($item), $items);
+        return $prepare->fetchAll();
     }
 
     public function livro($id){
-        $sql = "select * from livros";
-        $sql .= " where id = " . $id;
+        $prepare = $this->db->prepare("select * from livros where id = :id");
 
-        $query = $this->db->query($sql);
+        $prepare->bindValue('id', $id);
 
-        $items = $query->fetchAll();
+        // Do the map according to the class. native way
+        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
+        $prepare->execute();
+
+        return $prepare->fetch();
     
-        return array_map(fn($item) => Livro::make($item), $items)[0];
+        // return array_map(fn($item) => Livro::make($item), $items)[0];
     }
 }
